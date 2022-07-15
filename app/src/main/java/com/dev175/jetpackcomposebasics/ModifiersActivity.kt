a6b36d3024816842e74429d6e1c4850981048347
Tehmoor Amjad <iamtehmooramjad@gmail.com>
2022-07-15 19:30:52 +0500

    Modifiers

---
+++ app/src/main/java/com/dev175/jetpackcomposebasics/ModifiersActivity.kt
@@ -0,0 +1,45 @@
+package com.dev175.jetpackcomposebasics
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dev175.jetpackcomposebasics.ui.theme.JetPackComposeBasicsTheme
+
+class ModifiersActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column (
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Cyan)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+                    ){
+
+                Text(text = "Hello", Modifier.clickable {})
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World")
+            }
+        }
+    }
+}
+
