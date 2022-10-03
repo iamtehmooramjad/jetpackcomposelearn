63e1daaee8f8bbf2c6a11134db0405c809f317cc
Tehmoor Amjad <iamtehmooramjad@gmail.com>
2022-10-03 10:17:57 +0500

    Constraint Layout

---
+++ app/src/main/java/com/dev175/jetpackcomposebasics/LearnConstraintLayoutActivity.kt
@@ -0,0 +1,55 @@
+package com.dev175.jetpackcomposebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+
+class LearnConstraintLayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            val constraints = ConstraintSet {
+
+                val greenBox = createRefFor("greenBox")
+                val redBox = createRefFor("redBox")
+                val guideLine = createGuidelineFromTop(0.5f)
+
+                constrain(greenBox){
+                    top.linkTo(guideLine)
+                    start.linkTo(parent.start)
+                    width =  Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.SpreadInside)
+            }
+
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.background(Color.Green).layoutId("greenBox"))
+                Box(modifier = Modifier.background(Color.Red).layoutId("redBox"))
+            }
+        }
+    }
+}
+
